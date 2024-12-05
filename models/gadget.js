7c5d36ac0603b5e689cf718bc8f05ee7f3624c9a
chandanaaluri <chandanaaluri05@gmail.com>
2024-12-04 20:03:18 -0600

    updated

--- models/gadget.js
+++ models/gadget.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gadgetSchema = new Schema({
-  name: { type: String, required: true },
-  brand: { type: String, required: true },
-  price: { type: Number, required: true },
-  features: { type: String },
-  warranty: { type: String }
+  name: { type: String, required: true ,minlength: [3, 'Gadget name must be at least 3 characters long'], 
+    maxlength: [100, 'Gadget name cannot exceed 100 characters']},
+  brand: { type: String, required: true,minlength: [3, 'Gadget name must be at least 3 characters long'], 
+    maxlength: [100, 'Gadget name cannot exceed 100 characters'] },
+  price: { type: Number, required: true ,minlength: [3, 'Gadget name must be at least 3 characters long'], 
+    maxlength: [100, 'Gadget name cannot exceed 100 characters']},
+  features: { type: String ,minlength: [3, 'Gadget name must be at least 3 characters long'], 
+    maxlength: [100, 'Gadget name cannot exceed 100 characters']},
+  warranty: { type: String ,minlength: [3, 'Gadget name must be at least 3 characters long']}
 });
 
 module.exports = mongoose.model('Gadget', gadgetSchema);
